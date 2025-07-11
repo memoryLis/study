fef3d9846cab2cab0344427f13db36da1d617fcf
liang <liang@liang.com>
2025-07-11 19:42:29 +0800

    git 1 commit

---
+++ src/main/java/com/git/Main.java
@@ -0,0 +1,16 @@
+package com.git;
+
+/**
+ * ClassName: Main
+ * Package: com.git
+ * Description:
+ *
+ * @Author liang
+ * @Create 2025/7/11 19:41
+ * @Version jdk17.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
